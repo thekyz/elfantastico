99abdbe09acebd09cfdfe7d1bc76d33da273e77e
thekyz <thekyz@gmail.com>
2011-02-01 14:26:42 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elfantastico::Application.config.secret_token = '4ebeba110d958fb7303387c9af3aa6da4716206113ac768e34c23eb4c43f8740f727c68732b607f3f571b5c52ecb1a9e9e16dfedaeae78fe6c4e86830f944ea6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Elfantastico::Application.config.session_store :cookie_store, :key => '_elfantastico_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Elfantastico::Application.config.session_store :active_record_store
