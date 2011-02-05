ba3359c5731da604bb34fd99849911d38e866c15
thekyz <thekyz@gmail.com>
2011-02-05 12:55:51 -0800

    Added error check on book init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,17 @@ module ApplicationHelper
   def categories
     categorySet = Set.new []
     logger.info "----------- Getting categories"
+    book = Spreadsheet.open('data.xls')
     # Iterate through the category column
-    Spreadsheet.open('data.xls').worksheet(0).each 1 do |row|
-      # Try to add the category to the set
-      unless categorySet.add?(row[3]).nil?
-        logger.info "--- new category : " + row[3]
+    unless book.nil?
+      book.worksheet(0).each 1 do |row|
+        # Try to add the category to the set
+        unless categorySet.add?(row[3]).nil?
+          logger.info "--- new category : " + row[3]
+        end
       end
+    else
+      return []
     end
 
     # Return the set as array
