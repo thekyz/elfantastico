12b2d80f9e9a2c29eb92e03bc6e79f507c89c279
thekyz <thekyz@gmail.com>
2011-02-05 03:39:57 -0800

    Corrected title generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Aldrick &amp; Claire - Everything must go!"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
+
